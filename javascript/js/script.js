3ab8f8e02386215a93210222605103bfa6950ebd
JGonzales <70186093+JG0nzales@users.noreply.github.com>
2020-09-29 11:06:47 +0800

    Add files via upload

---
+++ javascript/js/script.js
@@ -0,0 +1,9 @@
+function checkInput(){
+var input = document.getElementById("txtIn");
+  //to display inputs codes
+//console.log(input.value);
+  //alert(input.value)
+  var notification = document.getElementById("txtNotif");
+  notification.innerHTML = input.value;
+}
+
